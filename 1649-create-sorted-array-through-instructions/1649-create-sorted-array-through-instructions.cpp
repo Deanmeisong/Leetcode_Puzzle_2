36e9e0920a3f61f9e3b81f46f4d2408107df3015
Deanmeisong <deanmeisong@gmail.com>
2024-04-05 00:15:43 +1100

    Time: 780 ms (33.19%), Space: 242.1 MB (33.19%) - LeetHub

--- 1649-create-sorted-array-through-instructions/1649-create-sorted-array-through-instructions.cpp
+++ 1649-create-sorted-array-through-instructions/1649-create-sorted-array-through-instructions.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     int smaller[100005];
-    int sorted[100005];
     int count[100005];
-    int m = 1e9+7;
-    int createSortedArray(vector<int>& A) {
-        int n = A.size();
-        for(int i = 0; i < n; ++i) sorted[i] = A[i];
-        helper(A, 0, n-1);
+    int sorted[100005];
+    int m = 1e9 + 7;
+    int createSortedArray(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i < n; ++i) sorted[i] = nums[i];
+        helper(nums, 0, n-1);
         int ret = 0;
         for(int i = 0; i < n; ++i) {
-            ret += min(smaller[i], i - smaller[i] - count[A[i]]);
+            ret += min(smaller[i], i - smaller[i] - count[nums[i]]);
             ret %= m;
-            count[A[i]]++;
+            count[nums[i]]++;
         }
         return ret;
     }
-    
-    void helper(vector<int>& A, int a, int b) {
+    void helper(vector<int>& nums, int a, int b) {
         if(a >= b) return;
         int mid = (b-a)/2 + a;
-        helper(A, a, mid);
-        helper(A, mid+1, b);
+        helper(nums, a, mid);
+        helper(nums, mid+1, b);
         for(int i = mid+1; i <= b; ++i) {
-            auto iter = lower_bound(sorted+a, sorted+mid+1, A[i]);
-            smaller[i] += iter-(sorted+a);
+            auto iter = lower_bound(sorted+a, sorted+mid+1, nums[i]);
+            smaller[i] += iter - (sorted+a);
         }
         inplace_merge(sorted+a, sorted+mid+1, sorted+b+1);
-        
     }
 };
