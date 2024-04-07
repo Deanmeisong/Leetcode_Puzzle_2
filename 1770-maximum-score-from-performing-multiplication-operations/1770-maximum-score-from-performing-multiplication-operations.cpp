648ac51e130e0007de43f5cd1fb2b64b972f113c
Deanmeisong <deanmeisong@gmail.com>
2024-04-07 11:00:49 +1000

    Time: 122 ms (40.55%), Space: 86 MB (70.80%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int maximumScore(vector<int>& nums, vector<int>& multipliers) {
         int n = nums.size(); int m = multipliers.size();
-        vector<vector<int>> dp(m+1, vector<int>(m+1));
-        int ans = INT_MIN;
-        for(int k = 1; k <= m; ++k)
-            for(int i = 0, j = k-i; i <= k; ++i, --j)
-                dp[i][j] = max(i ? dp[i-1][j] + nums[i-1]*multipliers[k-1] : INT_MIN, j ? dp[i][j-1] + nums[n-j]*multipliers[k-1] : INT_MIN);
-                
-        for(int i = 0; i <= m; ++i)
-            ans = max(ans, dp[i][m-i]);
-        return ans;
+        vector<vector<int>> cache(m, vector<int>(m, INT_MIN));
+        function<int(int,int)> dp = [&](int i, int j) {
+            int k = n - (j - i + 1);
+            if(k == m) return 0;
+            int& ans = cache[i][k];
+            if(ans != INT_MIN) return ans;
+            return ans = max(dp(i+1,j) + nums[i] * multipliers[k], dp(i,j-1) + nums[j] * multipliers[k]);
+        };
+        return dp(0, n-1);
     }
 };
