2ecebd470f34a499094592f9c3d7751801f6d394
Deanmeisong <deanmeisong@gmail.com>
2024-04-08 12:58:38 +1000

    Time: 318 ms (26.67%), Space: 107.6 MB (40.00%) - LeetHub

--- 1840-maximum-building-height/1840-maximum-building-height.cpp
+++ 1840-maximum-building-height/1840-maximum-building-height.cpp
@@ -6,15 +6,21 @@ public:
         int m = restrictions.size();
         vector<int> h(m);
         
-        for(int i = 1; i < m; ++i) h[i] = min(restrictions[i][1], h[i-1] + restrictions[i][0] - restrictions[i-1][0]);
-        for(int i = m-2; i >= 1; --i) h[i] = min(restrictions[i+1][0] - restrictions[i][0] + h[i+1], h[i]);
+        for(int i = 1; i < m; ++i)
+            h[i] = min(restrictions[i][1], h[i-1] + restrictions[i][0] - restrictions[i-1][0]);
         
-        int ret = 0;             
+        for(int i = m-2; i >= 0; --i)
+            h[i] = min(h[i], h[i+1] + restrictions[i+1][0] - restrictions[i][0]);
+        
+        int ret = 0;
+        
         for(int i = 1; i < m; ++i) {
-            int y = ((h[i-1]-h[i]) - (restrictions[i-1][0]-restrictions[i][0]))/2;
-            ret = max(ret, y+h[i]);
+            int x = (h[i-1]-h[i]-(restrictions[i-1][0]-restrictions[i][0]))/2;
+            ret = max(ret, x+h[i]);
         }
-        ret = max(ret, h[m-1]+(n-restrictions[m-1][0]));
+        
+        ret = max(ret, h[m-1] + (n-restrictions[m-1][0]));
         return ret;
+        
     }
 };
