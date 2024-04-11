5b0691bf219ecb407f83aa19467b4996b355333b
Deanmeisong <deanmeisong@gmail.com>
2024-04-11 21:00:30 +1000

    Time: 2532 ms (7.02%), Space: 263.4 MB (35.09%) - LeetHub

--- 1938-maximum-genetic-difference-query/1938-maximum-genetic-difference-query.cpp
+++ 1938-maximum-genetic-difference-query/1938-maximum-genetic-difference-query.cpp
@@ -2,72 +2,63 @@ class Solution {
     class TrieNode {
     public:
         TrieNode* next[2];
-        int count;
+        int cnt;
         TrieNode() {
-            count = 0;
             for(int i = 0; i < 2; ++i) next[i] = NULL;
+            cnt = 0;
         }
     };
-    
+public:
     TrieNode* root;
     vector<int> children[100005];
-    unordered_map<int,vector<pair<int,int>>> Map;
+    unordered_map<int, vector<pair<int,int>>> Map;
     vector<int> rets;
     
-public:
     vector<int> maxGeneticDifference(vector<int>& parents, vector<vector<int>>& queries) {
-        int n = parents.size();
-        rets.resize(queries.size());
-        int start;
-        for(int i = 0; i < n; ++i) {
-            if(parents[i] == -1) start = i;
+        int rootVal = -1;
+        for(int i = 0; i < parents.size(); ++i) {
+            if(parents[i] == -1) rootVal = i;
             else children[parents[i]].push_back(i);
         }
-        
-        for(int i = 0; i < queries.size(); ++i) {
-            int cur = queries[i][0];
-            Map[cur].push_back({queries[i][1], i});
-        }
+        rets.resize(queries.size());
         root = new TrieNode();
-        dfs(start);
+        for(int i = 0; i < queries.size(); ++i) Map[queries[i][0]].push_back({queries[i][1], i});
+        dfs(rootVal);
         return rets;
     }
     
-    void dfs(int cur) {
+    void dfs(int curVal) {
         TrieNode* node = root;
         for(int i = 31; i >= 0; --i) {
-            int d = ((cur>>i)&1);
+            int d = (curVal>>i)&1;
             if(node->next[d] == NULL) node->next[d] = new TrieNode();
             node = node->next[d];
-            node->count += 1;
+            node->cnt++;
         }
         
-        for(auto x : Map[cur]) {
-            int val = x.first; int idx = x.second;
-            TrieNode* node = root;
+        for(auto q : Map[curVal]) {
+            int val = q.first; int idx = q.second;
+            node = root;
             int ans = 0;
             for(int i = 31; i >= 0; --i) {
                 int d = (val>>i)&1;
-                if(node->next[1-d] != NULL && node->next[1-d]->count > 0) {
+                if(node->next[1-d] != NULL && node->next[1-d]->cnt > 0) {
                     node = node->next[1-d];
-                    ans = ans*2 + (1-d);
+                    ans = 2*ans + (1-d);
                 } else {
                     node = node->next[d];
-                    ans = ans*2 + (d);
+                    ans = 2*ans + (d);
                 }
             }
-            
             rets[idx] = ans^val;
         }
         
-        for(int child : children[cur]) dfs(child);
-        
+        for(int child : children[curVal]) dfs(child);
         node = root;
         for(int i = 31; i >= 0; --i) {
-            int d = (cur>>i)&1;
+            int d = (curVal>>i)&1;
             node = node->next[d];
-            node->count -= 1;
+            node->cnt-=1;
         }
-
     }
 };
