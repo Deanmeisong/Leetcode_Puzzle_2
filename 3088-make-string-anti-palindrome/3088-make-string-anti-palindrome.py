23e2744572f0f9d531594bf81de2b1dccad74015
dean <deanmeisong@gmail.com>
2024-05-11 14:02:07 +1000

    Time: 618 ms (61.06%), Space: 18.6 MB (71.68%) - LeetHub

--- 3088-make-string-anti-palindrome/3088-make-string-anti-palindrome.py
+++ 3088-make-string-anti-palindrome/3088-make-string-anti-palindrome.py
@@ -1,18 +1,12 @@
-class Solution(object):
-    def makeAntiPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
+class Solution:
+    def makeAntiPalindrome(self, s: str) -> str:
         n = len(s)
-        res = sorted(c for c in s)
-        
+        res = sorted(ch for ch in s)
         i = j = (n+1)//2
-        while j < n and res[j] == res[i]:
-            j += 1
+        while j < n and res[j] == res[i]: j+=1
         while res[i] == res[n-i-1]:
             if j == n: return "-1"
             res[i], res[j] = res[j], res[i]
-            j += 1
             i += 1
+            j += 1
         return "".join(res)
