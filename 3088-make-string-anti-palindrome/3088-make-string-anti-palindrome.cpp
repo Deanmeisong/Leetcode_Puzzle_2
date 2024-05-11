29cf989f9f1cdd370f800ef378037c40088692b3
dean <deanmeisong@gmail.com>
2024-05-11 14:05:02 +1000

    Time: 246 ms (25.25%), Space: 39.8 MB (93.94%) - LeetHub

---
+++ 3088-make-string-anti-palindrome/3088-make-string-anti-palindrome.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string makeAntiPalindrome(string s) {
+        int n = s.length();
+        int i = (n+1)/2; int j = (n+1)/2;
+        sort(s.begin(), s.end());
+        while(j < n && s[i] == s[j]) ++j;
+        while(s[i] == s[n-i-1]) {
+            if(j == n) return "-1";
+            swap(s[i], s[j]);
+            ++i; ++j;
+        }
+        return s;
+    }
+};
