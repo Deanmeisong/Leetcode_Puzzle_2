ace8c8fc0e74f03d21ce8c7e051c3a25fdfdf49b
dean <deanmeisong@gmail.com>
2024-05-12 11:13:11 +1000

    Time: 243 ms (95.89%), Space: 16.7 MB (97.95%) - LeetHub

--- 3104-find-longest-self-contained-substring/3104-find-longest-self-contained-substring.py
+++ 3104-find-longest-self-contained-substring/3104-find-longest-self-contained-substring.py
@@ -1,21 +1,15 @@
-class Solution(object):
-    def maxSubstringLength(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class Solution:
+    def maxSubstringLength(self, s: str) -> int:
         ans, n = -1, len(s)
         first, last = {}, {}
         for i, c in enumerate(s):
             if c not in first: first[c] = i
             last[c] = i
-        
         for c, i in first.items():
             mx = last[c]
             for j in range(i, n):
                 a, b = first[s[j]], last[s[j]]
                 if a < i: break
                 mx = max(mx, b)
-                if mx == j and j - i + 1 < n:
-                    ans = max(ans, j - i + 1)
-        return ans
+                if mx == j and j - i + 1 < n: ans = max(ans, j - i + 1)
+        return ans;
