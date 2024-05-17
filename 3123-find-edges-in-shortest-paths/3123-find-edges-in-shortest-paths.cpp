91f6e741be547c550f8c3b51746b2e4b4b8fc853
dean <deanmeisong@gmail.com>
2024-05-17 22:30:58 +1000

    Time: 1250 ms (19.36%), Space: 187.7 MB (46.50%) - LeetHub

--- 3123-find-edges-in-shortest-paths/3123-find-edges-in-shortest-paths.cpp
+++ 3123-find-edges-in-shortest-paths/3123-find-edges-in-shortest-paths.cpp
@@ -12,51 +12,56 @@ public:
         vector<int> dist1(n, INT_MAX/3);
         priority_queue<PII, vector<PII>, greater<>> pq;
         pq.push({0,0});
-        while(!pq.empty()) {
-            auto [d, cur] = pq.top(); pq.pop();
-            if(dist1[cur] == INT_MAX/3) dist1[cur] = d;
-            for(auto[nxt, w] : next[cur]) 
-                if(dist1[nxt] == INT_MAX/3) pq.push({w+d, nxt});
-        }
-        
-//         vector<int> dist2(n, INT_MAX/3);
-//         pq.push({0,n-1});
 //         while(!pq.empty()) {
 //             auto [d, cur] = pq.top(); pq.pop();
-//             if(dist2[cur] == INT_MAX/3) dist2[cur] = d;
-//             for(auto[nxt, w] : next[cur]) 
-//                 if(dist2[nxt] == INT_MAX/3) pq.push({w+d, nxt});
+//             if(dist1[cur] != INT_MAX/3) continue;
+//             dist1[cur] = d;
+//             for(auto[nxt, w] : next[cur]) {
+//                 if(dist1[nxt] != INT_MAX/3) continue;
+//                 pq.push({w+d, nxt});
+//             }
+               
 //         }
         
+        while (!pq.empty()) 
+        {
+            auto [dist, cur] = pq.top(); pq.pop();
+            if (dist1[cur] == INT_MAX/3) dist1[cur] = dist;                
+
+            for (auto [nxt, len]: next[cur])
+                if (dist1[nxt] == INT_MAX/3) pq.push({dist + len, nxt});
+            
+        }
+        
+        vector<int> dist2(n, INT_MAX/3);
+        pq.push({0,n-1});
+        while(!pq.empty()) {
+            auto [d, cur] = pq.top(); pq.pop();
+            if(dist2[cur] != INT_MAX/3) continue;
+                dist2[cur] = d;
+            for(auto[nxt, w] : next[cur]) {
+                if(dist2[nxt] != INT_MAX/3) continue;
+                pq.push({w+d, nxt});
+            }
+        }
+        
+
+        
+//         pq.push({0, n-1});        
+//         vector<int>dist2(n, INT_MAX/3);
 //         while (!pq.empty()) 
 //         {
 //             auto [dist, cur] = pq.top();
 //             pq.pop();
-//             if (dist1[cur]!= INT_MAX/3) continue;
-//             dist1[cur] = dist;                
+//             if (dist2[cur]!= INT_MAX/3) continue;
+//             dist2[cur] = dist;
 
 //             for (auto [nxt, len]: next[cur])
 //             {
-//                 if (dist1[nxt]!= INT_MAX/3) continue;                
+//                 if (dist2[nxt]!= INT_MAX/3) continue;                
 //                 pq.push({dist + len, nxt});
 //             }
 //         }
-        
-        pq.push({0, n-1});        
-        vector<int>dist2(n, INT_MAX/3);
-        while (!pq.empty()) 
-        {
-            auto [dist, cur] = pq.top();
-            pq.pop();
-            if (dist2[cur]!= INT_MAX/3) continue;
-            dist2[cur] = dist;
-
-            for (auto [nxt, len]: next[cur])
-            {
-                if (dist2[nxt]!= INT_MAX/3) continue;                
-                pq.push({dist + len, nxt});
-            }
-        }
         int m = edges.size();
         vector<bool> rets(m, false);
         for(int i = 0; i < m; ++i) {
