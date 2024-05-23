6f499810cb77721b0663ad4f531ed53f619b87ac
dean <deanmeisong@gmail.com>
2024-05-23 18:59:36 +1000

    Time: 1106 ms (39.33%), Space: 42.8 MB (59.55%) - LeetHub

--- 3130-find-all-possible-stable-binary-arrays-ii/3130-find-all-possible-stable-binary-arrays-ii.cpp
+++ 3130-find-all-possible-stable-binary-arrays-ii/3130-find-all-possible-stable-binary-arrays-ii.cpp
@@ -8,17 +8,17 @@ public:
         for(int i = 0; i <= zero; ++i)
             for(int j = 0; j <= one; ++j) {
                 if(i == 0 && j == 0) continue;
-                dp0[i][j] = i-1 < 0 ? 0 : presum1[j][i - 1] - (i-limit-1 < 0 ? 0 : presum1[j][i - limit - 1]);
+                dp0[i][j] = i-1 < 0 ? 0 : presum1[i-1][j] - (i-limit-1 < 0 ? 0 : presum1[i - limit - 1][j]);
                 dp1[i][j] = j-1 < 0 ? 0 : presum0[i][j - 1] - (j-limit-1 < 0 ? 0 : presum0[i][j - limit - 1]);
                 
                 dp0[i][j] = (dp0[i][j] + M) % M;
                 dp1[i][j] = (dp1[i][j] + M) % M;
                 
                 presum0[i][j] = (j<1?0:presum0[i][j-1]) + dp0[i][j];
-                presum1[j][i] = (i<1?0:presum1[j][i-1]) + dp1[i][j];
+                presum1[i][j] = (i<1?0:presum1[i-1][j]) + dp1[i][j];
 
                 
-                presum1[j][i] = ((i-1 < 0 ? 0 : presum1[j][i-1]) + dp1[i][j]) % M;
+                presum1[i][j] = ((i-1 < 0 ? 0 : presum1[i-1][j]) + dp1[i][j]) % M;
                 presum0[i][j] = ((j-1 < 0 ? 0 : presum0[i][j-1]) + dp0[i][j]) % M;
             }
         return (dp0[zero][one] + dp1[zero][one]) % M;
