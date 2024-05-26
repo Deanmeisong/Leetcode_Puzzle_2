45821b8625183fae7ef0d16404991a84cc47d9d7
dean <deanmeisong@gmail.com>
2024-05-26 23:38:20 +1000

    Time: 493 ms (5.38%), Space: 54 MB (52.15%) - LeetHub

---
+++ 2959-number-of-possible-sets-of-closing-branches/2959-number-of-possible-sets-of-closing-branches.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int numberOfSets(int n, int maxDistance, vector<vector<int>>& roads) {
+        int ret = 0;
+        for(int state = 0; state < (1<<n); ++state) {
+            vector<vector<int>> dp(n, vector<int>(n, INT_MAX/3));
+            for(int i = 0; i < n; ++i) 
+                if((state>>i)&1) dp[i][i] = 0;
+            for(const auto& road : roads) {
+                int a = road[0], b = road[1], w = road[2];
+                if((state>>a)&1==0) continue;
+                if((state>>b)&1==0) continue;
+                for(int i = 0; i < n; ++i) {
+                    if((state>>i)&1==0) continue;
+                    for(int j = 0; j < n; ++j) {
+                        if((state>>j)&1==0) continue;
+                        dp[i][j] = min(dp[i][j], dp[i][a] + w + dp[b][j]);
+                        dp[i][j] = min(dp[i][j], dp[i][b] + w + dp[a][j]);
+                    }
+                }    
+            }
+            int flag = 0;
+            for(int i = 0; i < n; ++i) {
+                if(((state>>i)&1)==0) continue;
+                for(int j = 0; j < n; ++j) {
+                    if(((state>>j)&1)==0) continue;
+                    if(dp[i][j] > maxDistance) {
+                        flag = 1;
+                        break;
+                    }
+                    if(flag) break;
+                }
+            }
+            if(flag == 0) ++ret;
+            
+        }
+        return ret;
+    }
+};
