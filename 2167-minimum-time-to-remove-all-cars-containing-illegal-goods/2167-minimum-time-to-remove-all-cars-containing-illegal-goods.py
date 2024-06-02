338c1f8aff15ded57d859db637642800860ab5c8
dean <deanmeisong@gmail.com>
2024-06-02 14:31:10 +1000

    Time: 1595 ms (16.66%), Space: 33.7 MB (30.00%) - LeetHub

--- 2167-minimum-time-to-remove-all-cars-containing-illegal-goods/2167-minimum-time-to-remove-all-cars-containing-illegal-goods.py
+++ 2167-minimum-time-to-remove-all-cars-containing-illegal-goods/2167-minimum-time-to-remove-all-cars-containing-illegal-goods.py
@@ -1,20 +1,16 @@
-class Solution(object):
-    def minimumTime(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class Solution:
+    def minimumTime(self, s: str) -> int:
         n = len(s)
-        pre = [0]*n
+        pre = [0] * n
         pre[0] = int(s[0] == '1')
         for i in range(n):
             pre[i] = pre[i-1] + int(s[i] == '1')
-        arr = [0]*n
+        arr = [0] * n
         for i in range(n):
             arr[i] = 2*pre[i] - i
-        preMax = 1
-        ret = n
+        
+        ret, preMax = n, 1
         for i in range(n):
             ret = min(ret, arr[i] - preMax + n)
-            preMax = max(arr[i], preMax)
+            preMax = max(preMax, arr[i])
         return ret
