23d9ca39020c8276601d0206b62464a1c7cb38df
dean <deanmeisong@gmail.com>
2024-06-02 14:39:11 +1000

    Time: 139 ms (56.73%), Space: 77.1 MB (39.42%) - LeetHub

--- 2167-minimum-time-to-remove-all-cars-containing-illegal-goods/2167-minimum-time-to-remove-all-cars-containing-illegal-goods.cpp
+++ 2167-minimum-time-to-remove-all-cars-containing-illegal-goods/2167-minimum-time-to-remove-all-cars-containing-illegal-goods.cpp
@@ -2,19 +2,21 @@ class Solution {
 public:
     int minimumTime(string s) {
         int n = s.size();
-        vector<int> presum(n);
-        presum[0] = (s[0] == '1');
-        for(int i = 1; i < n; ++i)
-            presum[i] = presum[i-1] + (s[i] == '1');
-        vector<int> arr(n);
-        for(int i = 0; i < n; ++i)
-            arr[i] = 2*presum[i] - i;
-        int preMax = 1;
-        int ret = n;
-        for(int i = 0; i < n; ++i) {
-            ret = min(arr[i] - preMax + n, ret);
-            preMax = max(preMax, arr[i]);
+        vector<int> left(n); vector<int> right(n);
+        left[0] = s[0]=='0'?0:1;
+        for(int i = 1; i < n; ++i) {
+            if(s[i] == '0') left[i] = left[i-1];
+            else left[i] = min(left[i-1]+2, i+1);
         }
+        right[n-1] = s[n-1]=='0'?0:1;
+        for(int j = n-2; j >= 0; --j) {
+            if(s[j] == '0') right[j] = right[j+1];
+            else right[j] = min(right[j+1]+2, n-j);
+        }
+        
+        int ret = min(left[n-1], right[0]);
+        for(int i = 0; i < n-1; ++i)
+            ret = min(ret, left[i]+right[i+1]);
         return ret;
     }
 };
