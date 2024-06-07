f6a815101f0004689a00dc89a12c870d74337bf2
dean <deanmeisong@gmail.com>
2024-06-07 19:59:22 +1000

    Time: 489 ms (58.26%), Space: 14.4 MB (64.68%) - LeetHub

---
+++ 1494-parallel-courses-ii/1494-parallel-courses-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minNumberOfSemesters(int n, vector<vector<int>>& relations, int k) {
+        vector<int> dp(1<<n, INT_MAX/2);
+        vector<int> preCourse(n, 0);
+        vector<int> preState(1<<n, 0);
+        for (auto x: relations)        
+            preCourse[x[1]-1] |= 1<<(x[0]-1);    
+        
+        for(int state = 0; state < (1<<n); state++) {
+            preState[state] = 0;
+            for(int i = 0; i < n; ++i)
+                if((state>>i)&1) preState[state] |= preCourse[i];
+            if(preState[state] == 0 && __builtin_popcount(state) <= k) dp[state] = 1;
+        }
+
+        dp[0] = 0;
+        for(int state = 0; state < (1<<n); state++) {
+            for(int subset = state; subset > 0; subset = (subset - 1)&state)
+            {
+                 if(__builtin_popcount(state) - __builtin_popcount(subset) <= k && (preState[state]&subset) == preState[state])
+                 {
+                     dp[state] = min(dp[state], dp[subset]+1);
+                 } 
+            }  
+        }
+        return dp[(1<<n)-1];
+    }
+};
