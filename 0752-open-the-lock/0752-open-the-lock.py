e930015c5eb7ae2d11fb598b21238978ae5cf3db
dean <deanmeisong@gmail.com>
2024-06-15 13:43:40 +1000

    Time: 533 ms (74.05%), Space: 12.7 MB (47.12%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.py
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -1,28 +1,23 @@
 class Solution(object):
     def openLock(self, deadends, target):
-        """
-        :type deadends: List[str]
-        :type target: str
-        :rtype: int
-        """
-        
         deadSet = set(deadends)
         if "0000" in deadSet: return -1
-        q = deque(["0000"])
         steps = 0
+        q = deque(["0000"])
         while q:
             for _ in range(len(q)):
                 curr = q.popleft()
                 if curr == target: return steps
-                for nei in self.neighbour(curr):
+                for nei in self.neigs(curr):
                     if nei in deadSet: continue
                     deadSet.add(nei)
                     q.append(nei)
-            steps += 1 
+            steps += 1
         return -1
-    def neighbour(self, code):
+    def neigs(self, code):
         for i in range(4):
             x = int(code[i])
             for diff in [-1,1]:
-                y = (x + diff + 10)%10
-                yield code[:i]+str(y)+code[i+1:]
+                y = (x+diff+10)%10
+                yield code[:i] + str(y) + code[i+1:]
+    
