c5c5a2349359e5c62a1f8a77de2e94c20f3e52ed
dean <deanmeisong@gmail.com>
2024-06-15 18:32:24 +1000

    Time: 62 ms (66.67%), Space: 51.4 MB (66.67%) - LeetHub

---
+++ 0738-monotone-increasing-digits/0738-monotone-increasing-digits.ts
@@ -0,0 +1,14 @@
+function monotoneIncreasingDigits(n: number): number {
+    let s = n.toString().split('');
+    let make = 0;
+    for(let i = s.length-1; i >= 1; --i) {
+        if(s[i] < s[i-1]) {
+            make = i;
+            s[i-1] = (parseInt(s[i-1])-1).toString();
+        }
+    }
+    if(make == 0) return n;
+    for(let i = make; i < s.length; ++i)
+        s[i] = '9';
+    return parseInt(s.join(''));
+};
