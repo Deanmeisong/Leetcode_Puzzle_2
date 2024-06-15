bab67cc16c6f7b992ae2913911880c92af44dcec
dean <deanmeisong@gmail.com>
2024-06-15 20:27:18 +1000

    Time: 2 ms (51.90%), Space: 7.1 MB (71.62%) - LeetHub

--- 0754-reach-a-number/0754-reach-a-number.cpp
+++ 0754-reach-a-number/0754-reach-a-number.cpp
@@ -2,11 +2,15 @@ class Solution {
 public:
     int reachNumber(int target) {
         target = abs(target);
-        int k = 0;
-        int sum = 0;
+        int k = sqrt(target*2);
+        int sum = getSum(k);
         while(sum < target) sum += (++k);
         const int d = sum - target;
         if(d % 2 == 0) return k;
         return (k+1) + (k%2);
+    }
+    
+    int getSum(int k) {
+        return (k+1)*k/2;
     }
 };
