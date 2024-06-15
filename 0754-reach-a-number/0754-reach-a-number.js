a8b9ad0ccd4a2dfeb69f8e14e1140677c695393d
dean <deanmeisong@gmail.com>
2024-06-15 20:44:23 +1000

    Time: 60 ms (21.74%), Space: 48.8 MB (47.83%) - LeetHub

---
+++ 0754-reach-a-number/0754-reach-a-number.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} target
+ * @return {number}
+ */
+var reachNumber = function(target) {
+    target = Math.abs(target);
+    let sum = 0; let k = 0;
+    while(sum < target) {
+        sum += (++k);
+    }
+    let d = sum - target;
+    if(d % 2 == 0) return k;
+    return k + 1 + (k%2);
+};
