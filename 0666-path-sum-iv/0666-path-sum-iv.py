4324e9bb96218a909d60d47626494a9eccbc130e
dean <deanmeisong@gmail.com>
2024-06-16 18:03:02 +1000

    Time: 35 ms (64.61%), Space: 16.6 MB (32.92%) - LeetHub

--- 0666-path-sum-iv/0666-path-sum-iv.py
+++ 0666-path-sum-iv/0666-path-sum-iv.py
@@ -1,16 +1,18 @@
-class Solution(object):
-    def pathSum(self, nums):
-        self.sum, self.tree = 0, {}
-        for x in nums:
-            self.tree[x//10] = x%10
-        self.dfs(nums[0]//10, 0)
-        return self.sum
-    def dfs(self, root, pre):
-        cur = pre + self.tree[root]
-        level, pos = root//10, root%10
-        left, right = (level+1)*10 + 2*pos-1, (level+1)*10 + 2*pos
-        if left not in self.tree and right not in self.tree:
-            self.sum += cur
-            return
-        if left in self.tree: self.dfs(left, cur)
-        if right in self.tree: self.dfs(right, cur)
+class Solution:
+    def pathSum(self, nums: List[int]) -> int:
+        sum, tree = 0, {}
+        for x in nums: tree[x//10] = x%10
+    
+        def dfs(root, pre):
+            nonlocal sum
+            cur = tree[root] + pre
+            lev, pos = root//10, root%10
+            left, right = (lev+1)*10+2*pos-1, (lev+1)*10+2*pos
+            if left not in tree and right not in tree:
+                sum += cur
+                return
+            if left in tree: dfs(left, cur)
+            if right in tree: dfs(right, cur)
+
+        dfs(nums[0]//10, 0)
+        return sum
