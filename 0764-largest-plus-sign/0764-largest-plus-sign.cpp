3f74ab7dcf44ade990c18f555a8421bc0df328b1
dean <deanmeisong@gmail.com>
2024-06-16 15:52:20 +1000

    Time: 67 ms (73.02%), Space: 36.7 MB (67.45%) - LeetHub

--- 0764-largest-plus-sign/0764-largest-plus-sign.cpp
+++ 0764-largest-plus-sign/0764-largest-plus-sign.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int orderOfLargestPlusSign(int n, vector<vector<int>>& mines) {
-        vector<vector<int>> matrix(n, vector<int>(n,1));
-        for(auto it : mines) 
-            matrix[it[0]][it[1]] = 0;
-
-        vector<vector<int>> left, right, top, down;
-        left = right = top = down = matrix;
-        for (int i = 0; i < n; ++i)
-            for(int j = 0; j < n; ++j) {
-                int x = n - 1 - i; int y = n - 1 - j;
-                if(i>0 && top[i][j]) top[i][j] = top[i-1][j]+1;
-                if(j>0 && left[i][j]) left[i][j] = left[i][j-1]+1;
-                if(x<n-1 && down[x][y]) down[x][y] = down[x+1][y]+1;
-                if(y<n-1 && right[x][y]) right[x][y] = right[x][y+1]+1;
+        vector<vector<int>> grid(n, vector<int>(n,n));
+        for(auto it : mines)
+            grid[it[0]][it[1]] = 0;
+        for(int i = 0; i < n; ++i) 
+            for(int j = 0, k = n-1, l = 0, r = 0, u = 0, d = 0; j < n; ++j, --k) {
+                grid[i][j] = min(grid[i][j], l = (grid[i][j] == 0 ? 0 : l+1));
+                grid[i][k] = min(grid[i][k], r = (grid[i][k] == 0 ? 0 : r+1));
+                grid[j][i] = min(grid[j][i], u = (grid[j][i] == 0 ? 0 : u+1));
+                grid[k][i] = min(grid[k][i], d = (grid[k][i] == 0 ? 0 : d+1));
             }
         int ans = 0;
         for(int i = 0; i < n; ++i)
             for(int j = 0; j < n; ++j)
-                ans = max(ans, min({left[i][j], right[i][j], top[i][j], down[i][j]}));
+                ans = max(ans, grid[i][j]);
         return ans;
     }
 };
