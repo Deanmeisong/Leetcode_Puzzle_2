b61cddddbc10977993cec93cac176f59e23f6b11
dean <deanmeisong@gmail.com>
2024-06-16 16:15:35 +1000

    Time: 1634 ms (52.63%), Space: 15.1 MB (84.21%) - LeetHub

---
+++ 0764-largest-plus-sign/0764-largest-plus-sign.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def orderOfLargestPlusSign(self, n, mines):
+        dp = [[n]*n for _ in range(n)]
+        for x, y in mines:
+            dp[x][y] = 0
+        
+        for i in range(n):
+            l = r = u = d = 0
+            for j in range(n):
+                k = n - j - 1
+                l = 0 if dp[i][j] == 0 else l+1
+                dp[i][j] = min(dp[i][j], l)
+                r = 0 if dp[i][k] == 0 else r+1
+                dp[i][k] = min(dp[i][k], r)
+                u = 0 if dp[j][i] == 0 else u+1
+                dp[j][i] = min(dp[j][i], u)
+                d = 0 if dp[k][i] == 0 else d+1
+                dp[k][i] = min(dp[k][i], d)
+        
+        ans = 0
+        for i in range(n):
+            for j in range(n):
+                ans = max(ans, dp[i][j])
+        
+        return ans
