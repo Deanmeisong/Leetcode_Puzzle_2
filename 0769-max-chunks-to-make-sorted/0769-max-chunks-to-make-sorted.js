ce5905528d19027f3140c6c8eeb244524254cd55
dean <deanmeisong@gmail.com>
2024-06-17 12:44:55 +1000

    Time: 64 ms (5.17%), Space: 48.4 MB (82.76%) - LeetHub

---
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var maxChunksToSorted = function(arr) {
+    if(arr == null || arr.length == 0) return 0;
+    for(let i = 1; i < arr.length; ++i)
+        arr[i] = Math.max(arr[i], arr[i-1])
+    let count = 0;
+    for(let i = 0; i < arr.length; ++i)
+        if(arr[i] === i) ++count;
+    return count;
+};
