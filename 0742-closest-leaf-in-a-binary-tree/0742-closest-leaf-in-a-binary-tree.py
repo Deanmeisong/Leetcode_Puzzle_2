3dd86be85fe5f02bc73a4ba149555fd646c5dc90
dean <deanmeisong@gmail.com>
2024-06-17 17:08:26 +1000

    Time: 32 ms (83.33%), Space: 13.1 MB (16.67%) - LeetHub

---
+++ 0742-closest-leaf-in-a-binary-tree/0742-closest-leaf-in-a-binary-tree.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def findClosestLeaf(self, root, k):
+ 
+        self.start = None
+        graph = collections.defaultdict(list)
+
+        def dfs(node, parent, val):
+            if node:
+                if node.val == val:
+                    self.start = node
+                if parent:
+                    graph[node].append(parent)
+                    graph[parent].append(node)
+                dfs(node.left, node, val)
+                dfs(node.right, node, val)
+
+        dfs(root, None, k)
+        
+        q = collections.deque([self.start])
+        seen = set(q)
+        
+        while q:
+            node = q.popleft()
+            if node:
+                if node.left is None and node.right is None:
+                    return node.val
+                for nei in graph[node]:
+                    if nei not in seen:
+                        seen.add(nei)
+                        q.append(nei)
+        
+        return 0
