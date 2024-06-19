7b6a1ac7fc4a7809f26dec1739b6076b61f516b9
dean <deanmeisong@gmail.com>
2024-06-19 21:23:34 +1000

    Time: 16 ms (83.97%), Space: 11.6 MB (47.67%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.py
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.py
@@ -1,5 +1,9 @@
-class Solution:
-    def matrixScore(self, grid: List[List[int]]) -> int:
+class Solution(object):
+    def matrixScore(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
         m = len(grid)
         n = len(grid[0])
         res = m * (1<<(n-1))
@@ -7,4 +11,3 @@ class Solution:
             same = sum(grid[i][0] == grid[i][j] for i in range(m))
             res += max(same, m-same) * (1<<(n-1-j))
         return res
-                            
