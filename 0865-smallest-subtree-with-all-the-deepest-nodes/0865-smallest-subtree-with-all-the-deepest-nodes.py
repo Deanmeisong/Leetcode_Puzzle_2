0e4656a73154cc1fea4ef2eaccddfb4aa44799ca
dean <deanmeisong@gmail.com>
2024-06-20 20:53:59 +1000

    Time: 20 ms (35.34%), Space: 11.7 MB (94.83%) - LeetHub

--- 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.py
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.py
@@ -1,15 +1,17 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
-        def dfs(root):
-            if not root: return (-1, None)
-            ld, l = dfs(root.left)
-            rd, r = dfs(root.right)
-            if ld == rd: return(ld+1, root)
-            return (ld + 1, l) if ld > rd else (rd + 1, r)
-        return dfs(root)[1]
+class Solution(object):
+    def subtreeWithAllDeepest(self, root):
+        return self.dfs(root)[1]
+    def dfs(self, root):
+        if not root: return (-1, None)
+        ld, l = self.dfs(root.left)
+        rd, r = self.dfs(root.right)
+        if ld == rd: return(ld+1, root)
+        return (ld + 1, l) if ld > rd else (rd + 1, r)
+
+        
