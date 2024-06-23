af1add7c5b2c86be8b100bc6168e77b5248341a6
dean <deanmeisong@gmail.com>
2024-06-23 22:50:18 +1000

    Time: 218 ms (81.59%), Space: 21 MB (28.21%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -1,9 +1,8 @@
-class Solution(object):
-    def numPairsDivisibleBy60(self, time):
+class Solution:
+    def numPairsDivisibleBy60(self, time: List[int]) -> int:
         res = 0
         c = [0] * 60
         for t in time:
             res += c[-t%60]
             c[t%60] += 1
         return res
-        
