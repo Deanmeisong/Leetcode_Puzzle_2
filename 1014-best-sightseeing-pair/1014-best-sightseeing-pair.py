f80c42165bc19435cbded6c1331d8f64491fc26e
dean <deanmeisong@gmail.com>
2024-06-25 19:03:23 +1000

    Time: 363 ms (44.30%), Space: 22.3 MB (46.05%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.py
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.py
@@ -1,8 +1,7 @@
-class Solution(object):
-    def maxScoreSightseeingPair(self, values):
+class Solution:
+    def maxScoreSightseeingPair(self, values: List[int]) -> int:
         iMax, ans = values[0], 0
         for i in range(1, len(values)):
             ans = max(ans, iMax + values[i]-i)
             iMax = max(iMax, values[i]+i)
         return ans
-        
