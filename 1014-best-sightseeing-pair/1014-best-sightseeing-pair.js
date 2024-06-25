7cf8820f5abe6b17d9124a783b7ce5240ab0e226
dean <deanmeisong@gmail.com>
2024-06-25 19:05:40 +1000

    Time: 69 ms (7.69%), Space: 54.2 MB (23.08%) - LeetHub

---
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} values
+ * @return {number}
+ */
+var maxScoreSightseeingPair = function(values) {
+    let iMax = values[0];
+    let ans = 0;
+    for(let i = 1; i < values.length; ++i) {
+        ans = Math.max(ans, iMax + values[i] - i);
+        iMax = Math.max(iMax, values[i] + i);
+    }
+    return ans;
+};
