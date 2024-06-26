d8c4ba5428a95531ac066a2e6e0fbe85f7e97e93
dean <deanmeisong@gmail.com>
2024-06-26 19:47:43 +1000

    Time: 4 ms (46.31%), Space: 7.4 MB (50.34%) - LeetHub

--- 1017-convert-to-base-2/1017-convert-to-base-2.cpp
+++ 1017-convert-to-base-2/1017-convert-to-base-2.cpp
@@ -2,20 +2,11 @@ class Solution {
 public:
     string baseNeg2(int n) {
         if(n == 0) return "0";
-        return helper(n, -2);
-    }
-    
-    string helper(int N, int K) {
-        vector<char> ans;
-        while(N) {
-            int r = N%K;
-            N /= K;
-            if(r < 0) {
-                r -= K;
-                N += 1;
-            }
-            ans.push_back(r + '0');
+        string s;
+        while(n) {
+            s = to_string(n&1) + s;
+            n = -(n>>1);
         }
-        return {rbegin(ans), rend(ans)};
+        return s;
     }
 };
