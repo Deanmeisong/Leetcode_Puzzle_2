5aa0609eb4935ad671ea6916932ae4ad14613e0f
dean <deanmeisong@gmail.com>
2024-06-27 23:55:21 +1000

    Time: 25 ms (8.80%), Space: 16 MB (12.50%) - LeetHub

--- 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.cpp
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.cpp
@@ -1,16 +1,15 @@
-/**
- * @param {number} k
- * @return {number}
- */
-var smallestRepunitDivByK = function(k) {
-    if (k % 10 !== 1 && k % 10 !== 3 && k % 10 !== 7 && k % 10 !== 9) return -1;
-    let ms = new Set();
-    let m = 0;
-    for (let i = 1; i <= k; ++i) {
-        m = (10 * m + 1) % k;
-        if (m === 0) return i;
-        if (ms.has(m)) return -1;
-        ms.add(m);
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) {
+        if(k%10 != 1 && k%10 != 3 && k%10 != 7 && k%10 != 9) return -1;
+        int m = 0;
+        unordered_set<int> ms;
+        for(int i = 1; i <= k; ++i) {
+            m = (m*10+1)%k;
+            if(m == 0) return i;
+            if(ms.count(m)) return -1;
+            ms.insert(m);
+        }
+        return -1;
     }
-    return -1;
 };
