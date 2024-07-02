794c1fc294acadc0c6d3c9e7339da28a6abb6e56
dean <deanmeisong@gmail.com>
2024-07-02 20:00:26 +1000

    Time: 48 ms (80.00%), Space: 51.1 MB (100.00%) - LeetHub

---
+++ 1033-moving-stones-until-consecutive/1033-moving-stones-until-consecutive.ts
@@ -0,0 +1,11 @@
+function numMovesStones(a: number, b: number, c: number): number[] {
+    const x = Math.min(a, Math.min(b, c));
+    const z = Math.max(a, Math.max(b, c));
+    const y = a+b+c-x-z;
+    let mi = 0; let ma = 0;
+    if(z-x > 2) {
+        mi = y-x < 3 || z-y < 3 ? 1 : 2;
+        ma = z-x-2;
+    }
+    return [mi, ma];
+};
