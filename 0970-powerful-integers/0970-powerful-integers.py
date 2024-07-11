94cd0be464c774c170d985e19c12cd9ef982986e
dean <deanmeisong@gmail.com>
2024-07-11 17:19:27 +1000

    Time: 42 ms (18.72%), Space: 16.6 MB (34.22%) - LeetHub

--- 0970-powerful-integers/0970-powerful-integers.py
+++ 0970-powerful-integers/0970-powerful-integers.py
@@ -1,6 +1,5 @@
-class Solution(object):
-    def powerfulIntegers(self, x, y, bound):
+class Solution:
+    def powerfulIntegers(self, x: int, y: int, bound: int) -> List[int]:
         xs = {x**i for i in range(20) if x**i < bound}
         ys = {y**j for j in range(20) if y**j < bound}
         return {x+y for x in xs for y in ys if x+y <= bound}
-        
