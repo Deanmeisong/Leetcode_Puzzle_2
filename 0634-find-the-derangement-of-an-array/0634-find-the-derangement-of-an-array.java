0cbaf1956d382b4cd6e89561fa314fe5b5c2480b
dean <deanmeisong@gmail.com>
2024-07-14 17:29:22 +1000

    Time: 8 ms (100.00%), Space: 40.3 MB (82.61%) - LeetHub

--- 0634-find-the-derangement-of-an-array/0634-find-the-derangement-of-an-array.java
+++ 0634-find-the-derangement-of-an-array/0634-find-the-derangement-of-an-array.java
@@ -3,10 +3,12 @@ class Solution {
         int mod = 1000000007;
         if(n == 0) return 1;
         if(n == 1) return 0;
-        long[] dp = new long[n+1];
-        dp[1] = 0; dp[2] = 1;
-        for(int i = 3; i <= n; ++i)
-            dp[i] = ((dp[i-1] + dp[i-2])*(i-1))%mod;
-        return (int)dp[n];
+        long dp1 = 0; long dp2 = 1;
+        for(int i = 3; i <= n; ++i) {
+            long dp = ((dp1 + dp2)*(i-1))%mod;
+            dp1 = dp2;
+            dp2 = dp; 
+        }
+        return (int)dp2;
     }
 }
