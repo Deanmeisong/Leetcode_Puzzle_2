7b3235ff0a43562d3f45bf8dbadf5cdac6087231
dean <deanmeisong@gmail.com>
2024-07-17 19:34:55 +1000

    Time: 41 ms (85.71%), Space: 19.4 MB (64.29%) - LeetHub

---
+++ 0663-equal-tree-partition/0663-equal-tree-partition.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def checkEqualTree(self, root):
+        seen = []
+        def dfs(node):
+            if not node: return 0
+            seen.append(node.val + dfs(node.left) + dfs(node.right))
+            return seen[-1]
+        total = dfs(root)
+        seen.pop()
+        for s in seen:
+            if 2 * s == total: return True
+        return False
+        
