a9af7f563f174e83aa06880ac1fd9844720dd5a3
dean <deanmeisong@gmail.com>
2024-07-18 13:34:25 +1000

    Time: 44 ms (100.00%), Space: 50.2 MB (66.67%) - LeetHub

---
+++ 0651-4-keys-keyboard/0651-4-keys-keyboard.ts
@@ -0,0 +1,11 @@
+function maxA(n: number): number {
+    let dp = new Array(n+1).fill(0);
+    for(let i = 1; i <= n; ++i) {
+        dp[i] = i;
+        for(let j = 1; j < i - 2; ++j) {
+            const total = dp[j] + dp[j] * (i-j-2);
+            dp[i] = Math.max(dp[i], total);
+        }
+    }
+    return dp[n];
+};
