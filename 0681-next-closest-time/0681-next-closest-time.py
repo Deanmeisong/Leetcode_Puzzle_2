5fa0efbc73792f131e59ed65f6c23a9b14168d1d
dean <deanmeisong@gmail.com>
2024-07-19 23:03:01 +1000

    Time: 30 ms (92.28%), Space: 16.7 MB (17.28%) - LeetHub

--- 0681-next-closest-time/0681-next-closest-time.py
+++ 0681-next-closest-time/0681-next-closest-time.py
@@ -1,5 +1,5 @@
-class Solution(object):
-    def nextClosestTime(self, time):
+class Solution:
+    def nextClosestTime(self, time: str) -> str:
         s = set(time)
         hour = int(time[0:2])
         minute = int(time[3:5])
@@ -7,8 +7,7 @@ class Solution(object):
             minute += 1
             if minute == 60:
                 minute = 0
-                hour = 0 if hour == 23 else hour + 1
+                hour = 0 if hour == 23 else hour+1
             time = "%02d:%02d" % (hour, minute)
-            if set(time) <= s:
-                return time
+            if set(time) <= s: return time
         return time
