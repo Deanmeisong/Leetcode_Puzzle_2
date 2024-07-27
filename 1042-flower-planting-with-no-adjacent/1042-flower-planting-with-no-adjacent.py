22025f90fe883b8383f922d1ac1bb17ed01c9cb5
dean <deanmeisong@gmail.com>
2024-07-27 13:22:05 +1000

    Time: 318 ms (80.77%), Space: 18.7 MB (92.31%) - LeetHub

---
+++ 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def gardenNoAdj(self, n, paths):
+        ans = [0] * n
+        g = [[] for i in range(n)]
+        
+        for (x, y) in paths:
+            g[x-1].append(y-1)
+            g[y-1].append(x-1)
+        
+        for i in range(n):
+            ans[i] = ({1, 2, 3, 4} - {ans[j] for j in g[i]}).pop()
+        
+        return ans
+
+        
