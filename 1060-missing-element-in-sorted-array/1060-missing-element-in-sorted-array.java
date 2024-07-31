dfc45d12132a3797386bdf6739707f00990c5b93
dean <deanmeisong@gmail.com>
2024-07-31 20:12:50 +1000

    Time: 1 ms (28.58%), Space: 49.3 MB (46.35%) - LeetHub

---
+++ 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingElement(int[] nums, int k) {
+        int n = nums.length;
+        for(int i=1; i < n; ++i) {
+            int gap = nums[i]-nums[i-1]-1;
+            if(gap >= k) return nums[i-1]+k;
+            k -= gap;
+        }
+        return nums[n-1]+k;
+    }
+}
